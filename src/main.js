1e37b36a3685cf7e43604a69ef49e2a15f30ddb2
Kevin <39208150+Kvrnn@users.noreply.github.com>
2020-07-10 12:49:32 -0700

    Setactivity

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 // This line MUST be first, for discord.js to read the process envs!
 require('dotenv').config();
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
+    client.user.setActivity("Ape Simulator | //", {
+        type: "PLAYING"
+    });
     console.log("I am ready!");
 });
 
@@ -18,24 +22,11 @@ client.on("message", message => {
 
     // And our 2 real basic commands!
     if(command === 'ping') {
+        message.delete()
         message.channel.send('Pong!');
     } else
     if (command === 'blah') {
         message.channel.send('Meh.');
-    }
-});
-
-client.on("message", message => {
-    if (message.author.bot) return;
-    // The process.env.PREFIX is your bot's prefix in this case.
-    if (message.content.indexOf(process.env.PREFIX) !== 0) return;
-    // This is the usual argument parsing we love to use.
-    const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    // And our 2 real basic commands!
-    if(command === 'play') {
-        message.channel.send('Pong!');
     }
 });
 
